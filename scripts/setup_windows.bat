a2e5722977e5b357a68ce34b5a77c655987be73a
Piasy <xz4215@gmail.com>
2025-03-11 09:19:17 +0800

    fix windows ci

--- scripts/setup_windows.bat
+++ scripts/setup_windows.bat
@@ -11,5 +11,5 @@ copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc.dll libs\windows\
 copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc.def libs\windows\x64\ && ^
 copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc_api.h libs\windows\x64\ && ^
 cd kmp-webrtc\build\bin\mingwX64\releaseShared && ^
-%LIB_EXE% /def:kmp_webrtc.def /machine:x64 /out:kmp_webrtc.lib && ^
+"%LIB_EXE%" /def:kmp_webrtc.def /machine:x64 /out:kmp_webrtc.lib && ^
 copy /Y kmp_webrtc.lib ..\..\..\..\..\libs\windows\x64\
