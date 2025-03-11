fec5ac6b746b0e9584b3bae736db0a51bd3b31c9
Piasy <xz4215@gmail.com>
2025-03-11 09:07:40 +0800

    try again

--- scripts/setup_windows.bat
+++ scripts/setup_windows.bat
@@ -1,9 +1,15 @@
 @echo off
 
+if "%~1"=="" (
+    set "LIB_EXE=lib"
+) else (
+    set "LIB_EXE=%~1"
+)
+
 .\gradlew :kmp-webrtc:linkReleaseSharedMingwX64 && ^
 copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc.dll libs\windows\x64\ && ^
 copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc.def libs\windows\x64\ && ^
 copy /Y kmp-webrtc\build\bin\mingwX64\releaseShared\kmp_webrtc_api.h libs\windows\x64\ && ^
 cd kmp-webrtc\build\bin\mingwX64\releaseShared && ^
-lib /def:kmp_webrtc.def /machine:x64 /out:kmp_webrtc.lib && ^
+%LIB_EXE% /def:kmp_webrtc.def /machine:x64 /out:kmp_webrtc.lib && ^
 copy /Y kmp_webrtc.lib ..\..\..\..\..\libs\windows\x64\
